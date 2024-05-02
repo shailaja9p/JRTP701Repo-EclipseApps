2d0774f275d61c420be61a0026c86fa3b98da1d1
shailaja <donthulashailaja@gmail.com>
2024-05-02 19:51:59 +0530

    TL modified APP.java

--- MavenProj01/src/main/java/com/nt/service/App.java
+++ MavenProj01/src/main/java/com/nt/service/App.java
@@ -9,6 +9,12 @@ public class App
 	public int sum(int x, int y) {
 		return x+y;
 	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	public int mul(int x, int y) {
+		return x*y;
+	}
     public static void main( String[] args )
     {
     	App app = new App();
