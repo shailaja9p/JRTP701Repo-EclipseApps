4cc54bd773af943252718d4f0a6b6233426b516d
shailaja <donthulashailaja@gmail.com>
2024-05-03 12:43:13 +0530

    "TL created Payment branch and added UPI_Payment class

---
+++ MavenProj01/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,10 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+
+
+	public String doPayment(long phoneNo,double amount) {
+		System.out.println("Payment is done");
+		return "payment done"; 
+	}
+}
