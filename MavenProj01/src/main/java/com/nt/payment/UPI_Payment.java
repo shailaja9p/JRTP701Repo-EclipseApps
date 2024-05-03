3621516966297f417ba0b3bef9e03b7a0d076121
shailaja <donthulashailaja@gmail.com>
2024-05-03 12:40:57 +0530

    "TL removed paymment

--- MavenProj01/src/main/java/com/nt/payment/UPI_Payment.java
+++
@@ -1,9 +0,0 @@
-package com.nt.payment;
-
-public class UPI_Payment {
-
-	public String doPayment(long phoneNo,double amount) {
-		System.out.println("Payment is done");
-		return "payment done"; 
-	}
-}
