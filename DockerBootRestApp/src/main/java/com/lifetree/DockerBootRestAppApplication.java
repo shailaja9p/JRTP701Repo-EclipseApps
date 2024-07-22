2ed5ff677bdf9556c686e9127f5f7c5f645784d0
shailaja <donthulashailaja@gmail.com>
2024-07-22 12:39:48 +0530

    Project added to git

---
+++ DockerBootRestApp/src/main/java/com/lifetree/DockerBootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.lifetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBootRestAppApplication.class, args);
+	}
+
+}
---
+++ DockerBootRestApp/src/test/java/com/lifetree/DockerBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
