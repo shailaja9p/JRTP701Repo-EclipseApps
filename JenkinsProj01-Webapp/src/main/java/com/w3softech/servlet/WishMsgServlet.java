288c2abbc1ab37a62636fc0c9726e8d4dad058d3
shailaja <donthulashailaja@gmail.com>
2024-09-29 20:08:03 +0530

    servlet, jsp Webapplication deployed in tomcat9.x

--- JenkinsProj01-Webapp/src/main/java/com/w3softech/servlet/WishMsgServlet.java
+++ JenkinsProj01-Webapp/src/main/java/com/w3softech/servlet/WishMsgServlet.java
@@ -2,27 +2,31 @@ package com.w3softech.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
 
 @WebServlet("/wishUrl")
 public class WishMsgServlet extends HttpServlet {
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
 		response.setContentType("text/html");
 		PrintWriter pw= response.getWriter();
-		pw.println("<h1 style='color:red;text-align:center'>Good Morning</h1>");
+		pw.println("<h1 style='color:red;text-align:center'>Good Morning.........."+LocalDateTime.now()+"</h1>");
 		pw.println("<a href='index.jsp'>Go Home</a>");
 		pw.close();
 	}
 
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
 		doGet(request,response);
 	}
 }
--- JenkinsProj01-Webapp/src/main/webapp/index.jsp
+++ JenkinsProj01-Webapp/src/main/webapp/index.jsp
@@ -1,7 +1,4 @@
-
-
-
-<h1 style="color:red;text-align:center">Jenkins Web Application</h1>
+<h1 style="color:red;text-align:center">Welcome To Jenkins Web Application</h1>
 <br>
 <br>
 <a href="wishUrl"> Show wish message</a>
